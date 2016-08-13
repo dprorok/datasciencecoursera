f715a14e3eaeb17c2fe944cdf03143e9c8f98a5d
Dan Prorok <dan@prorok.cc>
2016-08-13 17:26:15 +0800

    cleaned up run_analysis.R

--- cleaningdataclass/run_analysis.R
+++ cleaningdataclass/run_analysis.R
@@ -1,114 +1,93 @@
-#Remember to source("project.R")
+library(dplyr)
 
-getData <- function(fileURL = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-                    destfile = "./dataset.zip") {
-     download.file(fileURL, destfile, method = "curl")
+## Download and unzip the dataset:
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+destfile <- "./dataset.zip"
+
+if (!file.exists(destfile)){
+     download.file(fileURL, destfile, method="curl")
      dateDownloaded <- Sys.Date()
-}
-
-prepareDatasets <- function() {
-
-     library(plyr)
-
-     # Prepare a list of activity labels to use as activity factor levels.
-     activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE)
-     activity_factor_levels <- activity_labels[,2]
-
-
-     # Read in y_train and y_test. Code the activity data with useful factor names from 
-     # activity_factor_labels. Give columns a useful name.
-     # This meets Requirement 3: Uses descriptive activity names to name the activities in the data set
-     y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", colClasses="factor")
-     y_train$V1 <- mapvalues(y_train$V1, from = levels(y_train$V1), to = activity_factor_levels)
-     names(y_train) <- "activity"
-
-     y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", colClasses="factor")
-     y_test$V1 <- mapvalues(y_test$V1, from = levels(y_test$V1), to = activity_factor_levels)
-     names(y_test) <- "activity"
-
-
-     # Read in the feature labels.
-     features <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
-
-
-     # Read in the training and test datasets, X_train.txt and X_test.txt, respectively.
-     # Give them meaningful column names from features.
-     # This meets Requirement 4: Appropriately labels the data set with descriptive variable names.
-     x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", stringsAsFactors=FALSE)
-     names(x_train) <- features[,2]
-
-     x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", stringsAsFactors=FALSE)
-     names(x_test) <- features[,2]
-
-
-     # Read in training subject IDs. Add column to identify dataset (train).
-     # Give the columns meaningful names.
-     # Repeat for the test dataset.
-     # This further meets Requirement 4: Appropriately labels the data set with descriptive variable names.
-     subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", colClasses="factor")
-     subject_train <- cbind(subject_train, rep("train",length(subject_train$V1)))
-     names(subject_train) <- c("subject", "dataset")
-
-     subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", colClasses="factor")
-     subject_test <- cbind(subject_test, rep("test",length(subject_test$V1)))
-     names(subject_test) <- c("subject", "dataset")
-
-
-     # Combine columns from the training and test datasets
-     training_dataset <- cbind(subject_train, y_train, x_train)
-     test_dataset <- cbind(subject_test, y_test, x_test)
-     
-     complete_dataset <- rbind(training_dataset, test_dataset)
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+     unzip(destfile) 
 }
 
 
-extractMeanMeasurements <- function(data) {
-     grep("mean", names(data))
-}
+# Prepare a list of activity labels to use as activity factor levels.
+activity.labels <- read.table("./UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE)
+activity.factor.levels <- activity.labels[,2]
 
 
-run_analysis <- function() {
-     # Complete Requirement 1: Merges the training and the test sets to create one data set.
-     # Complete Requirement 3: Uses descriptive activity names to name the activities in the data set
-     # Complete Requirement 4: Appropriately labels the data set with descriptive variable names.
-     data <- prepareDatasets()
+# Read in the labels from y_train and y_test. Code the activity data with useful factor names from 
+# activity_factor_labels. Give columns a useful name.
+# This completes Requirement 3: Uses descriptive activity names to name the activities in the data set
+train.labels <- read.table("./UCI HAR Dataset/train/y_train.txt", colClasses="factor")
+train.labels$V1 <- mapvalues(train.labels$V1, from = levels(train.labels$V1), to = activity.factor.levels)
+names(train.labels) <- "activity"
 
-     # data is a large data.frame with 10299 observations (7352 from training and 2947 from testing)
-     # and 564 variables. These variables include:
-     #    * subject (from subject_*.txt)
-     #    * dataset (factor of either "train" or "test", to indicate which dataset the data originated from)
-     #    * activity (a factor with meaninful names extracted from activity_labels.txt)
-     #    * the other 561 measurement variables from x_test.txt and x_train.txt and labeled
-     #         with meaningful names from features.txt
-     
-     # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-     # 5. From the data set in step 4, creates a second, independent tidy data set with the 
-     #       average of each variable for each activity and each subject.
-}
+test.labels <- read.table("./UCI HAR Dataset/test/y_test.txt", colClasses="factor")
+test.labels$V1 <- mapvalues(test.labels$V1, from = levels(test.labels$V1), to = activity.factor.levels)
+names(test.labels) <- "activity"
 
-# be sure to create a readme
-#
-# explain why this is tidy
-# reference Hadley's paper
-# articulate the tidy data criteria
-# no NAs: sum(is.na.data.frame(data)) == 0 returns TRUE
-# 1. Each variable forms a column
-# 2. Each observation forms a row
-# 3. Each table/file stores data about one kind of observation (in this case, sensor data
-# from a Samsung blah blah phone used by different people/subjects)
-#
-# explain in the readme the pupose of all files (readme, R file, codebook, tidy data)
-# explain how to read in the tidy dataset
-#
-#    data <- read.table(file_path, header = TRUE) #if they used some other way of saving the file than a default write.table, this step will be different
-#    View(data)
-#
-# explain that we are taking the most inclusive approach by including both data where 
-# mean() and std() are at the end of the variable name and variables where mean is somewhere in
-# the middle. This approach was chosen because the requirement was ambiguous and while it is
-# trivial to quickly remove columns from a tidy dataset if more data than was desired is included,
-# it is more involved fo the end user of the data to reconstruct data that was missing, but
-# desired. As such, all conceivable variables were included with the expectation that superfuous
-# ones can be removed by the user if desired.
-#
-# include a codebook with every variable defined
+# Read in the feature labels.
+features <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
+
+# Read in the training and test datasets
+test.measurements <- read.table("./UCI HAR Dataset/test/X_test.txt", stringsAsFactors=FALSE)
+train.measurements <- read.table("./UCI HAR Dataset/train/X_train.txt", stringsAsFactors=FALSE)
+
+# Give measurements meaningful column names from features.
+# This completes Requirement 4: Appropriately labels the data set with descriptive variable names.
+names(test.measurements) <- features[,2]
+names(train.measurements) <- features[,2]
+
+# Read in subject IDs.
+test.subject <- read.table("./UCI HAR Dataset/test/subject_test.txt", colClasses="factor")
+train.subject <- read.table("./UCI HAR Dataset/train/subject_train.txt", colClasses="factor")
+
+# Add column to identify dataset (test or train)
+test.subject <- cbind(test.subject, rep("test",length(test.subject$V1)))
+train.subject <- cbind(train.subject, rep("train",length(train.subject$V1)))
+
+# Give the columns meaningful names.
+# This further completes Requirement 4: Appropriately labels the data set with descriptive variable names.
+names(test.subject) <- c("subject", "dataset")
+names(train.subject) <- c("subject", "dataset")
+
+# Combine columns from the training and test datasets
+train.dataset <- cbind(train.subject, train.labels, train.measurements)
+test.dataset <- cbind(test.subject, test.labels, test.measurements)
+
+# Combine train and test datasets and then sort by subject and activity
+# This completes Requirement 1: Merges the training and the test sets to create one data set.
+complete.dataset <- rbind(train.dataset, test.dataset)
+complete.dataset$subject <- as.integer(complete.dataset$subject)
+complete.dataset <- complete.dataset[ order(complete.dataset$subject, complete.dataset$activity), ]
+complete.dataset$subject <- as.factor(complete.dataset$subject)
+
+# The below completes Requirement 2: Extracts only the measurements on the mean and 
+#    standard deviation for each measurement.
+# Choosing the broadest interpretation of "mean" to include calculations that are means
+# (typically ending in "mean()") as well as calculations based on means, such as
+# those ending in "meanFreq()" or including means such as "gravityMean." The intent
+# was unclear so I opted for the broadest interpretation, figuring that it's easier to
+# later subset out superfluous columns than it is to reclean the data to put incorrectly
+# deleted data back in.
+cols.mean <- grep("[Mm][Ee][Aa][Nn]", names(complete.dataset))
+cols.std <- grep("[Ss][Tt][Dd]", names(complete.dataset))
+
+# col 1 = subject; col 2 = dataset; col 3 = activity
+# we don't need to know originl dataset, so it is dropped below
+cols.to.extract <- c(1, 3, cols.mean, cols.std)
+cols.to.extract <- sort(cols.to.extract)
+extracted.data <- complete.dataset[,cols.to.extract] 
+
+# Calculate the means of each of the mean an std columns in extracted.data,
+# grouped by subject and activity.
+# This completes Requirement 5: From the data set in step 4, creates a second, independent 
+#      tidy data set with the average of each variable for each activity and each subject.
+tidy.data <- aggregate(extracted.data[,names(extracted.data) != c('subject','activity')],
+                       by=list(activity = extracted.data$activity, subject = extracted.data$subject),
+                       mean);
+
+write.table(tidy.data, "tidy.txt", row.names = FALSE, quote = FALSE)
